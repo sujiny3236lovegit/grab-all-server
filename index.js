e945ba3a458739827678ed5530e02481a9f946aa
sujiny3236lovegit <69749672+sujiny3236lovegit@users.noreply.github.com>
2021-02-23 23:04:39 +0900

    setting

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 var http = require('http');
 var hostname = '127.0.0.1';
-var port = 3000;
+var port = 8080;
+
+const server = http.createServer(function(req, res){
+  const path = req.url;
+  const method = req.method;
+  if(path === '/products'){
+    if(method === 'GET'){
+      res.writeHead(200, { "Content-Type": "application" });
+      const products = JSON.stringify([
+        {
+          name: "노트북",
+          price: 500000,
+        }
+      ]);
+      res.end(products);
+    }else if(method === 'POST'){
+      res.end("생성되었습니다.");
+    }
+  }
+  res.end("Good Bye");
+});
+
+server.listen(port, hostname);
+
+console.log('pangpang market server on!');
